c5235458d82cc5e66e337f04fc53785451fea597
Charline MONCOUCUT <cmoncouc@e1r1p12.42.fr>
2017-03-17 15:29:30 +0100

    littles stars on film

--- app/containers/Film.jsx
+++ app/containers/Film.jsx
@@ -3,6 +3,7 @@ import Video from "react-h5-video";
 import MovieBox from '../components/MovieBox';
 import styles from '../css/components/film';
 import classNames from 'classnames/bind';
+import StarRatingComponent from 'react-star-rating-component';
 
 const cx = classNames.bind(styles);
 
@@ -14,7 +15,14 @@ class Film extends Component {
             <div className={cx('filmContainer')}>
                 <h1 className={cx('title')}>  {this.props.title} </h1>
                 rating : <h1>  {this.props.rating} </h1>
-                genres : <h2>  {this.props.genres} </h2>
+                <StarRatingComponent
+                  name=""
+                  value={parseFloat(this.props.rating)}
+                  starCount={10}
+                  editing={false}
+                  emptyStarColor= {'#d5d5d5'}
+                  />
+                genres : <h2> {this.props.genres} </h2>
                 synopsis : <h3 className={cx('synopsis')}>  {this.props.synopsis} </h3>
                 language : <h2>  {this.props.language} </h2>
                 <MovieBox subFr={this.props.subFr} subEn={this.props.subEn} idImdb={this.props.idImdb}/>
