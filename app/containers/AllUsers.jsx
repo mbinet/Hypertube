2a0ad7d45fb530e6ade7076611d9552c6297d890
Chayaline <cha.moncoucut@gmail.com>
2017-03-27 15:03:35 +0200

    all users done

--- app/containers/AllUsers.jsx
+++ app/containers/AllUsers.jsx
@@ -6,34 +6,51 @@ import { Card, Layout, Button } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 import { Link } from 'react-router';
 
+function UserList(props){
+    console.log(props)
+    if (!props.users)
+    {
+        console.log('no users')
+        return (<p> No users </p>)
+    }
+    return (
+        <div className={cx('cards')}> {
+            props.users.map(function(user, i){
+                return (
+                    <Card key={i+1} style={{ width: 200, margin:5 }} bodyStyle={{ padding: 0}} >
+                        <div className={cx('custom-image')}>
+                            <img alt="example" width="100%" src={user.profile.picture} />
+                        </div>
+                        <div className={cx('custom-card')}>
+                            <h3>{user.profile.username}</h3>
+                            <p>{user.profile.firstname}</p>
+                            <p>{user.profile.lastname}</p>
+                        </div>
+                    </Card>
+                )
+            })}
+            </div>
+        )
+    }
+
 class AllUsers extends Component {
     constructor(props) {
         super(props)
         this.state = {
             users: props.allUsers,
         }
-        console.log("component props", props.allUsers, typeof props.allUsers)
     }
 
 componentWillReceiveProps(nextProps)  {
         var result = JSON.parse(nextProps.allUsers)
-        console.log("nextprops", result)
         this.setState({users: result})
     }
 
-    usersList (){
-        if (this.state.users){
-            this.state.users.forEach(user => {
-                return( <div>
-                    <p>user</p>
-                    <p>{user.email}</p>
-                </div>)
-            })
-        }
-    }
+
     render() {
+        var users = this.state.users
         return (
-            <userList />
+            <UserList users={users}/>
         )
     }
 }
