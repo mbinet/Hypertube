0bd5ec211904811421fdc938b3fa897accb06b1a
Charline MONCOUCUT <cmoncouc@e1r12p16.42.fr>
2017-03-29 17:35:10 +0200

    wrong user fixed

--- app/containers/User.jsx
+++ app/containers/User.jsx
@@ -20,20 +20,31 @@ class Film extends Component {
 
     render() {
         var btn = ""
-        if (this.props.cookieUser._id == this.props.user._id) {
+        if (this.props.user && this.props.cookieUser._id == this.props.user._id) {
             btn = <Link to={ '/user/' + this.props.user._id + '/update' }><Button type='primary' className={cx('button')} style={{ width: 100, marginBottom: 30, textAlign: 'center' }}>{trad.updatelol}</Button></Link>
+        }
+        var username = "user not found"
+        var firstname = ""
+        var lastname = ""
+        var imgsrc="https://uos.edu.pk/assets/backend/images/staff/imagenotfound.svg"
+
+        if (this.props && this.props.user){
+            var username = this.props.user.profile.username
+            var firstname = this.props.user.profile.firstname
+            var lastname = this.props.user.profile.lastname
+            var imgsrc = this.props.user.profile.picture
         }
         return (
             <Layout style={{ backgroundColor: 'white'}}>
                 <Content style={{ margin: 'auto' }}>
                     <Card style={{ width: 250 }} bodyStyle={{ padding: 0 }} >
                         <div className={cx('custom-image')}>
-                            <img alt="example" width="100%" src={this.props.user.profile.picture} />
+                            <img alt="example" width="100%" src={imgsrc} />
                         </div>
                         <div className={cx('custom-card')}>
-                            <h3>{this.props.user.profile.username}</h3>
-                            <p>{this.props.user.profile.firstname}</p>
-                            <p>{this.props.user.profile.lastname}</p>
+                            <h3>{username}</h3>
+                            <p>{firstname}</p>
+                            <p>{lastname}</p>
                         </div>
                     </Card>
                     <div style={{ textAlign: 'center', marginTop: 20 }}>{btn}</div>
